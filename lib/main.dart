f26c4b2f7494b35279ca7bcb37660c8f73f40cc0
nevruzoglu <nevruzoglu@gmail.com>
2019-12-29 01:26:47 +0300

    Flutter Developer page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,42 +10,80 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  color: Colors.red,
-                  width: 100,
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/profile.jfif'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Burak Nevruzoğlu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 25,
+                  letterSpacing: 0.4,
+                  fontFamily: 'Lobster',
                 ),
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        color: Colors.yellow,
-                        width: 100,
-                        height: 100,
-                      ),
-                      Opacity(
-                        opacity: 0.4,
-                        child: Container(
-                          color: Colors.yellow,
-                          width: 100,
-                          height: 100,
-                        ),
-                      ),
-                    ],
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'SourceSans',
+                ),
+              ),
+              SizedBox(
+                height: 30,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '532 648 88 06',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSans',
+                      fontSize: 15,
+                    ),
                   ),
                 ),
-                Container(
-                  color: Colors.blue,
-                  width: 100,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'nevruzoglu@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSans',
+                      fontSize: 15,
+                    ),
+                  ),
                 ),
-              ],
-            ),
-          )),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
